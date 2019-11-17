c1f69892636566ab3ffc0171d96c578e9dd25590
JohannesGaessler <johannesg@5d6.de>
2019-11-17 21:20:35 +0100

    Fixed writing to log file

--- authentication-test/add/central_server_distribute.py
+++ authentication-test/add/central_server_distribute.py
@@ -2,17 +2,19 @@
 from time import sleep
 from os import remove
 
-server_log = open('jobs.log', 'a')
 
 while True:
     sleep(5.0)
     try:
         database = open('database', 'r')
     except Exception:  # Exception type different between Python 2/3
+        print('No new jobs found')
         continue
     read_lines = database.readlines()
+    server_log = open('jobs.log', 'a')
     for read_line in read_lines:
         print('Distributed job %s' % read_line)
         server_log.write('Distributed job %s' % read_line)
+    server_log.close()
     database.close()
     remove('database')
